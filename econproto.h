d93a293d70f07a172508659a3a6437e0cf17c19f
Benjamin Franzke <benjaminfranzke@googlemail.com>
2013-03-09 06:56:28 +0100

    econproto: Remove trailing whitespace

--- econproto.h
+++ econproto.h
@@ -26,13 +26,13 @@
 
 #include <stdint.h>
 
-#define ECON_PORTNUMBER			3620	
-#define ECON_MAGICNUM_SIZE		4		
-#define ECON_IPADDRESS_SIZE		4		
-#define ECON_UNIQINFO_LENGTH		6		
-#define	ECON_PROJNAME_MAXLEN		32		
-#define ECON_KEYWORD_MAXLEN		16		
-#define ECON_ENCRYPTION_MAXLEN		8		
+#define ECON_PORTNUMBER			3620
+#define ECON_MAGICNUM_SIZE		4
+#define ECON_IPADDRESS_SIZE		4
+#define ECON_UNIQINFO_LENGTH		6
+#define	ECON_PROJNAME_MAXLEN		32
+#define ECON_KEYWORD_MAXLEN		16
+#define ECON_ENCRYPTION_MAXLEN		8
 
 
 #ifdef LITTLE_ENDIAN_HOST
@@ -54,82 +54,82 @@
 
 
 
-#define ECON_MAGIC_NUMBER	"EEMP"		
-#define ECON_PROTO_VERSION	"0100"		
-#define ECON_PROTOVER_MAXLEN	4			
+#define ECON_MAGIC_NUMBER	"EEMP"
+#define ECON_PROTO_VERSION	"0100"
+#define ECON_PROTOVER_MAXLEN	4
 
 
 enum e_proj_state {
-	E_PSTAT_NOUSE		=	1,			
-	E_PSTAT_USING		=	2,			
-	E_PSTAT_APPUSING	=	3			
+	E_PSTAT_NOUSE		=	1,
+	E_PSTAT_USING		=	2,
+	E_PSTAT_APPUSING	=	3
 };
 
 enum _e_EmpCommand {
-	E_CMD_EASYSEARCH	=	1,			
-	E_CMD_IPSEARCH		=	2,			
-	E_CMD_CLIENTINFO	=	3,			
-	E_CMD_REQCONNECT	=	4,			
-	E_CMD_CONNECTED		=	5,			
-	E_CMD_REQRESTART	=	6,			
-	E_CMD_FINISHRESTART	=	7,			
-	E_CMD_DISCONCLIENT	=	8,			
-	E_CMD_INTERRUPT		=	9,			
-	E_CMD_KEEPALIVE		=	10,			
+	E_CMD_EASYSEARCH	=	1,
+	E_CMD_IPSEARCH		=	2,
+	E_CMD_CLIENTINFO	=	3,
+	E_CMD_REQCONNECT	=	4,
+	E_CMD_CONNECTED		=	5,
+	E_CMD_REQRESTART	=	6,
+	E_CMD_FINISHRESTART	=	7,
+	E_CMD_DISCONCLIENT	=	8,
+	E_CMD_INTERRUPT		=	9,
+	E_CMD_KEEPALIVE		=	10,
 
-	E_CMD_SENDREQUESTS	=	12,			
-	E_CMD_CLIENTERROR	=	13,			
-	E_CMD_RESENDFULLSCRID	=	14,			
-	E_CMD_DISPLAYWAIT	=	15,			
-	E_CMD_SENDKEY		=	16, 
+	E_CMD_SENDREQUESTS	=	12,
+	E_CMD_CLIENTERROR	=	13,
+	E_CMD_RESENDFULLSCRID	=	14,
+	E_CMD_DISPLAYWAIT	=	15,
+	E_CMD_SENDKEY		=	16,
 
 	E_CMD_22		=	22,
 	E_CMD_25		=	25
 };
 
 enum _e_ProjType {
-	e_ptype_IM_X		=	1,			
-	e_ptype_IM_XP1		=	2,			
-	e_pType_IM_XP4		=	3			
+	e_ptype_IM_X		=	1,
+	e_ptype_IM_XP1		=	2,
+	e_pType_IM_XP4		=	3
 };
 
 enum _e_SendReq {
-	e_sendreq_OK		=	1,			
-	e_sendreq_NG		=	2			
+	e_sendreq_OK		=	1,
+	e_sendreq_NG		=	2
 };
 
 
 enum _e_Keyword {
-	e_keyword_nouse		=	0,			
-	e_keyword_use		=	1			
+	e_keyword_nouse		=	0,
+	e_keyword_use		=	1
 };
 
 
 
 enum _e_Encryption {
-	e_encrypt_nouse		=	0,			
-	e_encrypt_use		=	1			
+	e_encrypt_nouse		=	0,
+	e_encrypt_use		=	1
 };
 
 
 
 enum _e_Error {
-	e_error_keyword		=	0			
+	e_error_keyword		=	0
 };
 
 
 typedef struct {
-	uint8_t bitsPerPixel;		
-	uint8_t depth;			
-	uint8_t bigEndian;		
-	uint8_t trueColour;		
+	uint8_t bitsPerPixel;
+	uint8_t depth;
+	uint8_t bigEndian;
+	uint8_t trueColour;
 
-	uint16_t redMax;			
-	uint16_t greenMax;		
-	uint16_t blueMax;			
-	uint8_t redShift;			
-	uint8_t greenShift;		
-	uint8_t blueShift;		
+	uint16_t redMax;
+	uint16_t greenMax;
+	uint16_t blueMax;
+	uint8_t redShift;
+	uint8_t greenShift;
+	uint8_t blueShift;
 	uint8_t pad1;
 	uint16_t pad2;
 } rfbPixelFormat;
@@ -144,48 +144,48 @@ typedef struct {
 
 
 typedef struct {
-	char projName[ECON_PROJNAME_MAXLEN];		
-	uint8_t	projState;							
-	uint8_t	useKeyword;							
-	uint8_t	displayType;						
+	char projName[ECON_PROJNAME_MAXLEN];
+	uint8_t	projState;
+	uint8_t	useKeyword;
+	uint8_t	displayType;
 	uint8_t implicit_padding;
 	uint16_t unknown_field_1; /* value: 256 */
 } e_command_clientinfo;
 
 
 typedef struct {
-	uint8_t	useEncryption;						
+	uint8_t	useEncryption;
 	uint8_t	EncPassword[ECON_ENCRYPTION_MAXLEN];
-	uint8_t	subnetMask[ECON_IPADDRESS_SIZE];	
-	uint8_t	gateAddress[ECON_IPADDRESS_SIZE];	
+	uint8_t	subnetMask[ECON_IPADDRESS_SIZE];
+	uint8_t	gateAddress[ECON_IPADDRESS_SIZE];
 	uint8_t unknown_field_1; /* value: 0x02 */
 	uint8_t unknown_field_2; /* value: 0x01 */
 	uint8_t unknown_field_3; /* value: 0x03 */
-	rfbServerInitMsg vnesInitMsg;					
+	rfbServerInitMsg vnesInitMsg;
 } e_command_reqconnect;
 
 
 typedef struct {
-	char projName[ECON_PROJNAME_MAXLEN];		
-	uint8_t	projState;							
+	char projName[ECON_PROJNAME_MAXLEN];
+	uint8_t	projState;
 } e_command_connected;
 
 
 
 typedef struct {
-	uint8_t	errorNo;								
+	uint8_t	errorNo;
 } e_command_clienterror;
 
 
 
 typedef struct {
-	uint32_t resendID;								
+	uint32_t resendID;
 } e_command_resendfullscrid;
 
 
 
 typedef struct {
-	uint32_t keyID;                              
+	uint32_t keyID;
 } e_command_sendkey;
 
 typedef struct {
@@ -202,23 +202,23 @@ typedef struct {
 
 
 struct econ_header {
-	char magicnum[ECON_MAGICNUM_SIZE];		
-	char version[ECON_PROTOVER_MAXLEN];		
-	uint8_t IPaddress[ECON_IPADDRESS_SIZE];		
-	uint32_t commandID;							
-	uint32_t datasize;							
+	char magicnum[ECON_MAGICNUM_SIZE];
+	char version[ECON_PROTOVER_MAXLEN];
+	uint8_t IPaddress[ECON_IPADDRESS_SIZE];
+	uint32_t commandID;
+	uint32_t datasize;
 };
 
 struct econ_command {
-	uint8_t recordCount;				
+	uint8_t recordCount;
 	uint8_t implicit_padding[3];
 	union e_commands {
-		e_command_clientinfo clientinfo;	
-		e_command_reqconnect reqconnect;	
-		e_command_connected connected;		
-		e_command_clienterror clienterror;	
-		e_command_resendfullscrid resendid;	
-		e_command_sendkey sendkey;        
+		e_command_clientinfo clientinfo;
+		e_command_reqconnect reqconnect;
+		e_command_connected connected;
+		e_command_clienterror clienterror;
+		e_command_resendfullscrid resendid;
+		e_command_sendkey sendkey;
 
 		e_command_cmd22 cmd22;
 		e_command_cmd25 cmd25;
@@ -226,9 +226,9 @@ struct econ_command {
 };
 
 struct econ_record {
-	uint8_t projUniqInfo[ECON_UNIQINFO_LENGTH];	
-	uint8_t projKeyword[ECON_KEYWORD_MAXLEN];	
-	uint8_t IPaddress[ECON_IPADDRESS_SIZE];		
+	uint8_t projUniqInfo[ECON_UNIQINFO_LENGTH];
+	uint8_t projKeyword[ECON_KEYWORD_MAXLEN];
+	uint8_t IPaddress[ECON_IPADDRESS_SIZE];
 };
 
 #endif
